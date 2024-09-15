fd96d3b9a5d8509b030f5f4947ba7fa947706968
radian628 <70352880+radian628@users.noreply.github.com>
2024-09-14 22:33:14 -0700

    actually add in the css

--- src/index.tsx
+++ src/index.tsx
@@ -19,3 +19,10 @@ document.body.appendChild(root);
 const reactRoot = createRoot(root);
 
 reactRoot.render(<BetterWikidotEditor></BetterWikidotEditor>);
+
+const editorStylesheet = document.createElement("link");
+editorStylesheet.rel = "stylesheet";
+editorStylesheet.href =
+  "https://radian628.github.io/better-wikidot-editor/build/index.css";
+
+document.head.appendChild(editorStylesheet);
