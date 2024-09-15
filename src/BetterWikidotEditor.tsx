d7889c00d13aa5cf48eac4febb53a51483dabb88
radian628 <70352880+radian628@users.noreply.github.com>
2024-09-14 23:35:03 -0700

    only show bettereditor when editor is open

--- src/BetterWikidotEditor.tsx
+++ src/BetterWikidotEditor.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Editor } from "./editor/Editor";
 import "./BetterWikidotEditor.less";
 
 export function BetterWikidotEditor() {
   const [text, setText] = useState("asdf");
+  const [doesEditorExist, setDoesEditorExist] = useState(false);
+
+  useEffect(() => {
+    if (doesEditorExist) return;
+
+    const interval = setInterval(() => {
+      const textarea = document.querySelector(
+        "textarea#edit-page-textarea"
+      ) as HTMLTextAreaElement | null;
+
+      if (textarea) {
+        setDoesEditorExist(true);
+        setText(textarea.value);
+      }
+    });
+    return () => clearInterval(interval);
+  }, [doesEditorExist]);
+
+  if (!doesEditorExist) return <></>;
+
   return (
     <div className="better-wikidot-editor-container">
       <div className="editor-container">
